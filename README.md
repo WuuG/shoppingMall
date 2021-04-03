72ca0ee8a6d7639027138e11942833746575d829
Wuu-laptop <1249732457@qq.com>
2021-04-04 00:24:04 +0800

    2021.4.4 项目暂时告一段落

--- README.md
+++ README.md
@@ -17,3 +17,6 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## 2021.4.4 这个项目暂时告一段落。 之后再来完善
+## 后台接口，可以去B站底下添加老师微信获取
--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://152.136.185.210:7878/api/m5',
+    //后台接口，可以去B站底下添加老师微信获取
+    baseURL: 'http://152.136.185.210:',
     timeout: 5000,
   })
   return instance(config);
