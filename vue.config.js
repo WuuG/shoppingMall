0bba6c0a40d9ae4a2b6dcdf2c38977460fd47584
Wuu-Server <1249732457@qq.com>
2021-04-04 17:19:56 +0800

    部署配置

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const path = require("path");
 
 //会与默认配置合并
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/shoppingMall/'
+    : '/',
   //进行别名的设置
   configureWebpack: {
     resolve: {
