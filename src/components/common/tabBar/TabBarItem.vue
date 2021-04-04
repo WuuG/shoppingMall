86d9c2dcfa8521fd2d55b2221b4a5bfde601d3c4
Wuu-Server <1249732457@qq.com>
2021-04-04 11:10:01 +0800

    添加图片懒加载，响应式布局

--- src/components/common/tabBar/TabBarItem.vue
+++ src/components/common/tabBar/TabBarItem.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="tab-bar-item" @click="toPath(path)" :style="isActive">
-    <div v-show="isShow">
+    <div v-show="isShow" class="tab-bar-img">
       <slot name="img">
         <img src="~assets/img/tabbar/home.svg" alt="" />
       </slot>
     </div>
-    <div v-show="!isShow">
+    <div v-show="!isShow" class="tab-bar-img">
       <slot name="img-active">
         <img src="~assets/img/tabbar/home.svg" alt="" />
       </slot>
     </div>
     <!-- 在所用的template内，使用v-bind:传给父组件的参数(这里是可以自定义的)="子组件的参数"
     在本例中,我使用a给父组件传递这个参数 -->
-    <div>
+    <div class="tab-bar-text">
       <slot name="text" :a="home">{{ home.first.a }}</slot>
     </div>
   </div>
@@ -67,9 +67,16 @@ export default {
   flex: 1;
   text-align: center;
 }
+.tab-bar-img {
+  height: 30px;
+}
 .tab-bar-item img {
   width: 30px;
   height: 30px;
   vertical-align: top;
+}
+.tab-bar-text {
+  height: 19px;
+  line-height: 19px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import toast from './components/common/toast' //默认导入里面的index.js
+import VueLazyLoad from 'vue-lazyload'  //图片懒加载
+
 
 Vue.config.productionTip = false
 Vue.use(toast)  // 安装toast插件
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png') // 这里图片加载需要用require
+})
 
 Vue.prototype.$bus = new Vue();
 new Vue({
